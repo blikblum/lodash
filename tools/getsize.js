5addbe528b042b553fd3df9db7b63d6b49f30e9c
Luiz Américo <luizamericop@gmail.com>
2018-12-08 21:57:31 -0300

    Transform stats data into a simplified format

--- tools/getsize.js
+++ tools/getsize.js
@@ -3,9 +3,28 @@ const rollup = require('rollup')
 const minify = require('rollup-plugin-terser').terser
 const analyze = require('rollup-plugin-analyzer').plugin
 
+const extractIdRegex = /(\/|\\)([^\/\\]*)?\.js$/
+
+function parseStats(entry, stats) {
+  const result = {}
+  result.name = entry.substr(0, entry.length - 3)
+  result.size = stats.bundleSize
+  result.dependencies = stats.modules
+    .map(module => ({
+      name: module.id.match(extractIdRegex)[2],
+      size: module.size,
+      percent: module.percent,
+      internal: module.id.indexOf('.internal') !== -1,
+      dependents: module.dependents.map(dependent => dependent.match(extractIdRegex)[2])
+    }))
+    .filter(dependency => dependency.name !== result.name)
+
+  return result  
+}
+
 const entries = fs.readdirSync('.').filter(file => file.match(/^[^\.].*\.js$/))
 
-const results = {}
+const results = []
 
 async function build() {
   for (entry of entries) {
@@ -14,7 +33,7 @@ async function build() {
       plugins: [
         minify(), 
         analyze({onAnalysis: result => {
-          results[entry] = result
+          results.push(parseStats(entry, result))
         }})]
     });    
 

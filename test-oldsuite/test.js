83d630ba5140cdb2765151d7e673c7acd682a61a
Luiz Américo <luizamericop@gmail.com>
2019-08-16 12:20:12 -0300

    Remove strict mode tests

--- test-oldsuite/test.js
+++ test-oldsuite/test.js
@@ -4074,29 +4074,6 @@
 
   /*--------------------------------------------------------------------------*/
 
-  QUnit.module('strict mode checks');
-
-  lodashStable.each(['defaults', 'defaultsDeep', 'merge'], function(methodName) {
-    var func = _[methodName],
-        isBindAll = methodName == 'bindAll';
-
-    QUnit.test('`_.' + methodName + '` should ' + (isStrict ? '' : 'not ') + 'throw strict mode errors', function(assert) {
-      assert.expect(1);
-
-      var object = freeze({ 'a': undefined, 'b': function() {} }),
-          pass = !isStrict;
-
-      try {
-        func(object, isBindAll ? 'b' : { 'a': 1 });
-      } catch (e) {
-        pass = !pass;
-      }
-      assert.ok(pass);
-    });
-  });
-
-  /*--------------------------------------------------------------------------*/
-
   QUnit.module('lodash.filter');
 
   (function() {
